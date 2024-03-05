5310bc2c1649101b1acd20d15862476ae3c4428f
Sandross <sandrosalasfilho3@gmail.com>
2024-03-05 19:15:15 -0300

    feat: 100% test coverage

--- test/vehicles-allocation/vehicles-allocation.service.spec.ts
+++ test/vehicles-allocation/vehicles-allocation.service.spec.ts
@@ -9,7 +9,15 @@ import { getRepositoryToken } from '@nestjs/typeorm';
 import { VehiclesAllocationEntity } from '../../src/modules/vehicles-allocation/vehicle-allocation.entity';
 import { VehiclesEntity } from '../../src/modules/vehicles/vehicle.entity';
 import { DriverEntity } from '../../src/modules/drivers/driver.entity';
+import * as checks from '../../src/modules/vehicles-allocation/utils';
+import { HttpException, HttpStatus } from '@nestjs/common';
 
+jest.mock('../../src/modules/vehicles-allocation/utils', () => ({
+  checkVehicleExists: jest.fn(),
+  checkDriverExists: jest.fn(),
+  checkIsVehicleAllocated: jest.fn(),
+  driverHasAlreadyActiveContract: jest.fn(),
+}));
 describe('VehiclesAllocationService', () => {
   let service: VehiclesAllocationService;
 
@@ -82,8 +90,19 @@ describe('VehiclesAllocationService', () => {
     }
   });
   it('should allocate vehicle', async () => {
+    // Mockando as funções de verificação
+    jest.spyOn(checks, 'checkVehicleExists').mockResolvedValueOnce();
+    jest.spyOn(checks, 'checkDriverExists').mockResolvedValueOnce();
+    jest.spyOn(checks, 'checkIsVehicleAllocated').mockResolvedValueOnce();
+    jest
+      .spyOn(checks, 'driverHasAlreadyActiveContract')
+      .mockResolvedValueOnce();
+
+    // Mockando o retorno para o primeiro findOne
     mockedVehiclesService.findOne.mockResolvedValueOnce({});
     mockedDriversService.findOne.mockResolvedValueOnce({});
+
+    // Mockando o retorno para o segundo findOne
     mockedVehicleAllocationService.findOne.mockResolvedValueOnce(null);
     mockedVehicleAllocationService.findOne.mockResolvedValueOnce(null);
 
@@ -107,9 +126,76 @@ describe('VehiclesAllocationService', () => {
       reason: 'test',
     };
 
-    await service.allocateVehicle(allocateVehicleSut);
     const result = await service.allocateVehicle(allocateVehicleSut);
 
-    expect(result).toEqual(vehicleAllocation);
+    expect(result).toEqual(expect.objectContaining(vehicleAllocation));
+  });
+  it('should throw error if allocateVehicle throws', async () => {
+    jest.spyOn(checks, 'checkVehicleExists').mockRejectedValueOnce({});
+    try {
+      await service.allocateVehicle({
+        vehicleId: '1',
+        driverId: '1',
+        reason: 'test',
+      });
+    } catch (error) {
+      expect(error.message).toEqual('Http Exception');
+    }
+  });
+  it('should throw if  finishVehiclesAllocateContract throws', async () => {
+    mockedVehicleAllocationService.findOne.mockRejectedValueOnce(
+      new Error('Vehicle allocation contract not found or its already done'),
+    );
+    const contractId = 1;
+    try {
+      await service.finishVehiclesAllocateContract(contractId);
+    } catch (error) {
+      expect(error.message).toBe(
+        'Vehicle allocation contract not found or its already done',
+      );
+    }
+  });
+
+  it('should throw if finishVehiclesAllocateContract get an error', async () => {
+    mockedVehicleAllocationService.findOne.mockRejectedValueOnce(
+      new Error('Vehicle allocation contract not found or its already done'),
+    );
+    const contractId = 1;
+    try {
+      await service.finishVehiclesAllocateContract(contractId);
+    } catch (error) {
+      expect(error.message).toBe(
+        'Vehicle allocation contract not found or its already done',
+      );
+    }
+  });
+  it('should throw if finishVehiclesAllocateContract throws', async () => {
+    // Mockando a chamada para findOne lançar um erro, simulando um erro interno
+    mockedVehicleAllocationService.findOne.mockImplementation(() => {
+      throw new Error(
+        'Vehicle allocation contract not found or its already done',
+      );
+    });
+
+    const contractId = 1;
+    try {
+      await service.finishVehiclesAllocateContract(contractId);
+    } catch (error) {
+      expect(error).toBeInstanceOf(HttpException);
+      expect(error.message).toBe(
+        'Vehicle allocation contract not found or its already done',
+      );
+      expect(error.getStatus()).toBe(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  });
+  it('should throw if getAllAllocatedVehicles throws', async () => {
+    mockedVehicleAllocationService.find.mockRejectedValueOnce(
+      new Error('Internal'),
+    );
+    try {
+      await service.getAllAllocatedVehicles();
+    } catch (error) {
+      expect(error.message).toBe('Internal');
+    }
   });
 });
